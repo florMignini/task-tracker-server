9a797aa7c0c3319b1c49572f5f63f5a63366b1e6
florMignini <flormignini29@gmail.com>
2024-06-27 09:51:55 -0300

    single task model

---
+++ src/tasks/model/task.model.ts
@@ -0,0 +1,15 @@
+export interface Task {
+  id: string;
+  title: string;
+  description: string;
+  startDate?: Date;
+  deadline?: Date;
+  status: TaskStatus;
+}
+
+export enum TaskStatus {
+  TODO = 'TODO',
+  IN_PROGRESS = 'IN_PROGRESS',
+  DONE = 'DONE',
+  CANCELED = 'CANCELED',
+}
