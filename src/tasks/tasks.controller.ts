cadf0b968dd28e39a8d1001e7222c3252848f5a1
florMignini <flormignini29@gmail.com>
2024-07-01 19:48:01 -0300

    create task update

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { Task } from './model/task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private taskService: TasksService) {}
+
+  @Post()
+  createTask(createtaskDto: CreateTaskDto): Task {
+    return this.taskService.createTask(createtaskDto);
+  }
 }
