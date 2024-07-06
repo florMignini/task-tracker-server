04e9e15868fd59ca5555321ea736ce5df442b34f
florMignini <flormignini29@gmail.com>
2024-07-06 08:46:40 -0300

    task search by filter actions

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Get, Post, Query } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './model/task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { TaskFilterDto } from './dto/filter-task.dto';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private taskService: TasksService) {}
 
+  @Get()
+  getTaskByFilter(@Query() taskFilterDto: TaskFilterDto): Task[] {
+    if (Object.keys(taskFilterDto).length) {
+      return this.taskService.getTaskByFilter(taskFilterDto);
+    } else {
+      return this.taskService.getAllTasks();
+    }
+  }
   @Post()
   createTask(createtaskDto: CreateTaskDto): Task {
     return this.taskService.createTask(createtaskDto);
