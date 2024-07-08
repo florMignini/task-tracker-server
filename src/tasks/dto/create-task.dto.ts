90cf0a698c9a3651448d8dc8cfa97f8bff5a849d
florMignini <flormignini29@gmail.com>
2024-07-08 20:21:53 -0300

    adding pipes

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,6 +1,16 @@
+import { IsDate, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
 export class CreateTaskDto {
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(6)
   title: string;
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(6)
   description: string;
+  @IsDate()
   startDate?: Date;
+  @IsDate()
   deadline?: Date;
 }
