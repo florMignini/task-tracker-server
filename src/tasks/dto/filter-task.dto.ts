2b9080b891fee0ed15d312f55015e5690018307b
florMignini <flormignini29@gmail.com>
2024-07-13 07:32:09 -0300

    task filter validation pipe

--- src/tasks/dto/filter-task.dto.ts
+++ src/tasks/dto/filter-task.dto.ts
@@ -1,7 +1,18 @@
+import { IsIn, IsNotEmpty, IsOptional } from 'class-validator';
 import { TaskStatus } from '../model/task.model';
 
 // TODO: add to DTO date parameter search
 export class TaskFilterDto {
+  @IsOptional()
+  @IsIn([
+    //*Allowed status array
+    TaskStatus.CANCELED,
+    TaskStatus.DONE,
+    TaskStatus.IN_PROGRESS,
+    TaskStatus.TO_DO,
+  ])
   status?: TaskStatus;
+  @IsOptional()
+  @IsNotEmpty()
   search?: string;
 }
