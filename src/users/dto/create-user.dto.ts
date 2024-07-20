95bb47bc0a78cab2be89acdb9a490d3f9fc0c2b7
florMignini <flormignini29@gmail.com>
2024-07-20 09:15:49 -0300

    create user dto

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,22 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  firstName: string;
+  @IsNotEmpty()
+  @IsString()
+  lastName: string;
+  @IsNotEmpty()
+  @IsString()
+  @IsEmail()
+  email: string;
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(6)
+  username: string;
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(6)
+  password: string;
+}
