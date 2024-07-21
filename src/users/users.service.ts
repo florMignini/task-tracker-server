031ec9c091d7f585e1a7e30a10a578914f0f80ef
florMignini <flormignini29@gmail.com>
2024-07-21 19:29:28 -0300

    user service update

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from './entities/user.entity';
 import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -9,4 +10,19 @@ export class UsersService {
     @InjectRepository(UserEntity)
     private readonly userRepository: Repository<UserEntity>,
   ) {}
+  public async createUser(newUser: CreateUserDto): Promise<UserEntity> {
+    try {
+      return await this.userRepository.save(newUser);
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  public async getAllUsers(): Promise<UserEntity[]> {
+    try {
+      return await this.userRepository.find();
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
 }
