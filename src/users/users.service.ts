6136c55ca3c51d39d9b11254364c6bbab517bdf0
florMignini <flormignini29@gmail.com>
2024-07-21 19:40:02 -0300

    update user service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,8 @@
+import { DeleteResult, Repository, UpdateResult } from 'typeorm';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from './entities/user.entity';
-import { Repository } from 'typeorm';
-import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto, CreateUserDto } from './dto';
 
 @Injectable()
 export class UsersService {
@@ -21,6 +21,33 @@ export class UsersService {
   public async getAllUsers(): Promise<UserEntity[]> {
     try {
       return await this.userRepository.find();
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  public async updateUser(
+    id: string,
+    updateUserDto: UpdateUserDto,
+  ): Promise<UpdateResult | undefined> {
+    try {
+      const user = await this.userRepository.update(id, updateUserDto);
+      if (!user) {
+        throw new Error('User not found');
+      }
+      return user;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  public async deleteUser(id: string): Promise<DeleteResult | undefined> {
+    try {
+      const user = await this.userRepository.delete(id);
+      if (!user) {
+        throw new Error('User not found');
+      }
+      return user;
     } catch (error) {
       throw new Error(error);
     }
