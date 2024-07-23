2da6df5161aee27d165eedd92c9435b47fd47e0c
florMignini <flormignini29@gmail.com>
2024-07-23 10:17:21 -0300

    get and post user controller methods

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  /* 
+  public
+  register user method
+  */
+  @Post('register')
+  public async register(@Body() createUserDto: CreateUserDto) {
+    return await this.usersService.createUser(createUserDto);
+  }
+
+  /* 
+  public
+  get all users method
+  */
+  @Get('get-all')
+  public async getAllUsers() {
+    return await this.usersService.getAllUsers();
+  }
+
+  /* 
+  public 
+  get single user by id method
+  */
+  @Get(':id')
+  public async getSingleUserById(@Param('id') id: string) {
+    return await this.usersService.getSingleUserById(id);
+  }
+}
