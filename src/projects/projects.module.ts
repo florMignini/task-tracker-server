7b3379b8eb97b0083e41085f097096783715ad89
florMignini <flormignini29@gmail.com>
2024-08-05 20:19:08 -0300

    project controller

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ProjectsService } from './projects.service';
+import { ControllersController } from './controllers.controller';
 
 @Module({
   providers: [ProjectsService],
+  controllers: [ControllersController],
 })
 export class ProjectsModule {}
