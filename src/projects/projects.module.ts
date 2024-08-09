8bc2e417635afdbfe8c530c1b2ca75d299242c45
florMignini <flormignini29@gmail.com>
2024-08-09 07:57:47 -0300

    create project

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ProjectsService } from './projects.service';
 import { ControllersController } from './controllers.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProjectEntity } from './entities/projects.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ProjectEntity])],
   providers: [ProjectsService],
   controllers: [ControllersController],
 })
--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -1,4 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ProjectEntity } from './entities/projects.entity';
+import { Repository } from 'typeorm';
+import { CreateProjectDto } from './dto/create-project.dto';
+import { ErrorHandler } from 'utils/error-handler';
 
 @Injectable()
-export class ProjectsService {}
+export class ProjectsService {
+  constructor(
+    @InjectRepository(ProjectEntity)
+    private readonly projectRepository: Repository<ProjectEntity>,
+  ) {}
+  public async createProject(
+    createProjectDto: CreateProjectDto,
+  ): Promise<ProjectEntity> {
+    try {
+      //checking if project already exist in DB
+      const existingProject = await this.projectRepository.findOne({
+        where: {
+          name: createProjectDto.title,
+        },
+      });
+      if (existingProject) {
+        throw new ErrorHandler({
+          type: 'CONFLICT',
+          message: 'Project already exists',
+        });
+      } else {
+        const newProject =
+          await this.projectRepository.create(createProjectDto);
+        return await this.projectRepository.save(newProject);
+      }
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+}
