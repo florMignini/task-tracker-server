2e5b4b9a46e5bcade2b8114318ce3300ff7efaeb
florMignini <flormignini29@gmail.com>
2024-08-12 17:13:56 -0300

    projects controller update

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ProjectsService } from './projects.service';
-import { ControllersController } from './controllers.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProjectEntity } from './entities/projects.entity';
+import { ProjectsController } from './projects.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ProjectEntity])],
   providers: [ProjectsService],
-  controllers: [ControllersController],
+  controllers: [ProjectsController],
 })
 export class ProjectsModule {}
